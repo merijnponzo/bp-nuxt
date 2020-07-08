ad8bf6f8c7ce11ab0fa4e20aed11b3807964d6a2
Merijn <merijn@ponzo.nl>
2020-07-08 12:56:48 +0200

    middleware 500
    middleware 500

--- middleware/delay.js
+++ middleware/delay.js
@@ -4,6 +4,6 @@ export default ({ isServer }) => {
   // Return a promise to tell nuxt.js to wait for the end of it
   return new Promise((resolve) => {
     // Wait 1 second between each route
-    setTimeout(resolve, 200);
+    setTimeout(resolve, 500);
   })
 }
