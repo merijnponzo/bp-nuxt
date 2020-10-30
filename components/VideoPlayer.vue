82f86ab23f87cd7ab7c1e51ddf01936f13ff6bdf
merren <merijn@ponzo.nl>
2020-10-30 12:25:01 +0100

    playinline
    playinline

--- components/VideoPlayer.vue
+++ components/VideoPlayer.vue
@@ -1,95 +1,94 @@
 <style lang="scss" scoped>
-  #bpplayer{
-    width:100%!important;
-    height:auto!important;
-  }
+#bpplayer {
+  width: 100% !important;
+  height: auto !important;
+}
 </style>
 <template>
   <client-only>
-    <div class="video" :class="outer"> 
-      <div :class="[ratio,inner]">
+    <div class="video" :class="outer">
+      <div :class="[ratio, inner]">
         <video
-            v-if="windowWidth"
-            loop
-            autoplay
-            :controls="showcontrols"
-            ref="bpplayersingle"
-            class="wid--fl"
-            muted=""
-            :key="desktop"
-            >
-            <template v-if="windowWidth < 1000">
-              <source :src="mobile" type="video/mp4">
-            </template>
-            <template v-else-if="desktop">
-              <source :src="desktop" type="video/mp4">
-            </template>
-            <template v-else>
-              <source :src="mobile" type="video/mp4">
-            </template>
-          </video>
+          v-if="windowWidth"
+          loop
+          autoplay
+          playinline
+          ref="bpplayersingle"
+          class="wid--fl"
+          muted=""
+          :key="desktop"
+        >
+          <template v-if="windowWidth < 1000">
+            <source :src="mobile" type="video/mp4" />
+          </template>
+          <template v-else-if="desktop">
+            <source :src="desktop" type="video/mp4" />
+          </template>
+          <template v-else>
+            <source :src="mobile" type="video/mp4" />
+          </template>
+        </video>
       </div>
     </div>
-    </client-only>
+  </client-only>
 </template>
 
 <script>
-import Vue from 'vue';
-import VueWindowSize from 'vue-window-size';
+import Vue from "vue";
+import VueWindowSize from "vue-window-size";
 Vue.use(VueWindowSize);
 
-
 export default {
-  name: 'Video',
+  name: "Video",
   props: {
     poster: {
       type: [Object, Boolean],
-      default: function () {
-        return {}
+      default: function() {
+        return {};
       }
     },
-    showcontrols:{
+    showcontrols: {
       type: [String, Boolean],
       default: false
     },
-    outer:{
-      type:String,
-      default:''
-    },
-    inner:{
-      type:String,
-      default:''
-    },
-    mobile:{
-      type: [String, Boolean],
-      default: null
-    },
-    desktop:{
-      type: [String, Boolean],
-      default: null
-    },
-    ratio:{
+    outer: {
       type: String,
-      default:'ratio--4x3'
+      default: ""
+    },
+    inner: {
+      type: String,
+      default: ""
+    },
+    mobile: {
+      type: [String, Boolean],
+      default: null
+    },
+    desktop: {
+      type: [String, Boolean],
+      default: null
+    },
+    ratio: {
+      type: String,
+      default: "ratio--4x3"
     }
   },
-  data : function(){
+  data: function() {
     return {
-      play : false
-    }
+      play: false
+    };
   },
-  methods:{
-    autoPlay(e){
-      if(e.percentInView > 0.85 && !this.play){
-        this.play = true
-        const player = this.$refs['bpplayersingle']
-        if(player){
-          player.play()
+  methods: {
+    autoPlay(e) {
+      if (e.percentInView > 0.85 && !this.play) {
+        this.play = true;
+        const player = this.$refs["bpplayersingle"];
+        if (player) {
+          player.play();
         }
-      } else if (e.type === 'exit' ){
-        this.play = false
+      } else if (e.type === "exit") {
+        this.play = false;
       }
     }
   }
-}
+};
 </script>
